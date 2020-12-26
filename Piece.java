fbe702f3f65762e4f10bec90f4e01ba60e2f1f50
CGuitel <cecile.guitel@gmail.com>
2020-12-26 16:51:40 +0100

    suppression des vieux fichiers

--- Piece.java
+++
@@ -1,5 +0,0 @@
-public abstract class Piece {
-	public abstract String toString();
-	public abstract int getType();
-	// On avait pensé à déclarer clone() ici, mais on ne peut alors plus accéder à super.clone() dans Animal et Bloc
-}
