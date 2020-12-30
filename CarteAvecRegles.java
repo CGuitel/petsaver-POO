a7fddbded7c1488c780e8e9b480a80051e1c0148
Valentina-Fed <76586010+Valentina-Fed@users.noreply.github.com>
2020-12-30 17:06:39 +0100

    Add files via upload

--- CarteAvecRegles.java
+++ CarteAvecRegles.java
@@ -1,15 +1,36 @@
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class CarteAvecRegles extends Carte {
 
 	CarteAvecRegles(String chemin) {
 		super(chemin);
-		JTextArea zoneText = new JTextArea(20, 30);
+		JTextArea zoneText = new JTextArea(readFile("regles.txt"));
 		JScrollPane texteAsc = new JScrollPane(zoneText);
 		texteAsc.setVerticalScrollBarPolicy(texteAsc.VERTICAL_SCROLLBAR_ALWAYS);
 		super.imageFon.add(texteAsc);
 		zoneText.setEditable(true);
 		zoneText.setVisible(true);
 	}
+    public String readFile( String file )
+    {
+        // lit le fichier
+        String lines = "";
+        String line;
+        try
+        {
+            BufferedReader reader = new BufferedReader( new FileReader(file) );
+            while( (line = reader.readLine()) != null )
+                lines += line+"\n";
+        }
+        catch( Exception e )
+        {
+            lines = "Une erreur s'est produite durant la lecture : "+e.getMessage();
+        }  
+         
+        return lines;
+    }
 }
--- CarteNouveauJoueur.java
+++ CarteNouveauJoueur.java
@@ -1,4 +1,32 @@
+import java.awt.Dimension;
+import java.awt.Font;
+import java.text.NumberFormat;
 
-public class CarteNouveauJoueur {
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
+public class CarteNouveauJoueur extends Carte {
+	
+	CarteNouveauJoueur (String chemin) {
+		super(chemin);
+		JTextField nomJoueur = new JTextField("");
+		//pour recuperer : nomJoueur.getText();
+		JLabel nomLabel = new JLabel ("Nom du joueur");
+		nomJoueur.setFont(new Font("freemono", Font.BOLD, 100));
+		nomJoueur.setPreferredSize(new Dimension(150, 30));
+		JLabel niveauLabel = new JLabel ("Niveau preferré");
+		JFormattedTextField niveau = new JFormattedTextField(NumberFormat.getIntegerInstance());
+		niveau.setFont(new Font("freemono", Font.BOLD, 100));
+		niveau.setPreferredSize(new Dimension(150, 30));
+		JButton b = new JButton ("OK");
+		//b.addActionListener(new BoutonListener() --> commencer le jeu);
+		this.imageFon.add(nomLabel);
+		this.imageFon.add(nomJoueur);
+		nomJoueur.setEditable(true);
+		this.imageFon.add(niveauLabel);
+		this.imageFon.add(niveau);
+		this.imageFon.add(b);
+	}
 }
